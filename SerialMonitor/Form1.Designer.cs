cc131b876de279bf41f76a0314b849281f134492
b-jasko <bjasko97@gmail.com>
2018-07-04 16:34:44 +0200

    Options tab design

--- SerialMonitor/Form1.Designer.cs
+++ SerialMonitor/Form1.Designer.cs
@@ -28,21 +28,258 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Terminal = new System.Windows.Forms.TabPage();
+            this.Opcje = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbName = new System.Windows.Forms.ComboBox();
+            this.cbBaud = new System.Windows.Forms.ComboBox();
+            this.cbDataBits = new System.Windows.Forms.ComboBox();
+            this.cbParityBits = new System.Windows.Forms.ComboBox();
+            this.cbStopBits = new System.Windows.Forms.ComboBox();
+            this.btnDefault = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.Opcje.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Terminal);
+            this.tabControl1.Controls.Add(this.Opcje);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(519, 560);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Terminal
+            // 
+            this.Terminal.Location = new System.Drawing.Point(4, 29);
+            this.Terminal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Terminal.Name = "Terminal";
+            this.Terminal.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Terminal.Size = new System.Drawing.Size(607, 531);
+            this.Terminal.TabIndex = 0;
+            this.Terminal.Text = "Terminal";
+            this.Terminal.UseVisualStyleBackColor = true;
+            // 
+            // Opcje
+            // 
+            this.Opcje.Controls.Add(this.btnCancel);
+            this.Opcje.Controls.Add(this.btnRefresh);
+            this.Opcje.Controls.Add(this.btnDefault);
+            this.Opcje.Controls.Add(this.cbStopBits);
+            this.Opcje.Controls.Add(this.cbParityBits);
+            this.Opcje.Controls.Add(this.cbDataBits);
+            this.Opcje.Controls.Add(this.cbBaud);
+            this.Opcje.Controls.Add(this.cbName);
+            this.Opcje.Controls.Add(this.label5);
+            this.Opcje.Controls.Add(this.label4);
+            this.Opcje.Controls.Add(this.label3);
+            this.Opcje.Controls.Add(this.label2);
+            this.Opcje.Controls.Add(this.label1);
+            this.Opcje.Location = new System.Drawing.Point(4, 29);
+            this.Opcje.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Opcje.Name = "Opcje";
+            this.Opcje.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Opcje.Size = new System.Drawing.Size(511, 527);
+            this.Opcje.TabIndex = 1;
+            this.Opcje.Text = "Opcje";
+            this.Opcje.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Bity stopu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bity przystości:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 216);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bity danych:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prędkość:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nazwa portu:";
+            // 
+            // cbName
+            // 
+            this.cbName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbName.FormattingEnabled = true;
+            this.cbName.Location = new System.Drawing.Point(328, 47);
+            this.cbName.Name = "cbName";
+            this.cbName.Size = new System.Drawing.Size(121, 28);
+            this.cbName.TabIndex = 5;
+            // 
+            // cbBaud
+            // 
+            this.cbBaud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbBaud.FormattingEnabled = true;
+            this.cbBaud.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "76800",
+            "115200",
+            "230400"});
+            this.cbBaud.Location = new System.Drawing.Point(328, 130);
+            this.cbBaud.Name = "cbBaud";
+            this.cbBaud.Size = new System.Drawing.Size(121, 28);
+            this.cbBaud.TabIndex = 6;
+            // 
+            // cbDataBits
+            // 
+            this.cbDataBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDataBits.FormattingEnabled = true;
+            this.cbDataBits.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.cbDataBits.Location = new System.Drawing.Point(328, 213);
+            this.cbDataBits.Name = "cbDataBits";
+            this.cbDataBits.Size = new System.Drawing.Size(121, 28);
+            this.cbDataBits.TabIndex = 7;
+            // 
+            // cbParityBits
+            // 
+            this.cbParityBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbParityBits.FormattingEnabled = true;
+            this.cbParityBits.Location = new System.Drawing.Point(328, 296);
+            this.cbParityBits.Name = "cbParityBits";
+            this.cbParityBits.Size = new System.Drawing.Size(121, 28);
+            this.cbParityBits.TabIndex = 8;
+            // 
+            // cbStopBits
+            // 
+            this.cbStopBits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStopBits.FormattingEnabled = true;
+            this.cbStopBits.Location = new System.Drawing.Point(328, 379);
+            this.cbStopBits.Name = "cbStopBits";
+            this.cbStopBits.Size = new System.Drawing.Size(121, 28);
+            this.cbStopBits.TabIndex = 9;
+            // 
+            // btnDefault
+            // 
+            this.btnDefault.Location = new System.Drawing.Point(79, 459);
+            this.btnDefault.Name = "btnDefault";
+            this.btnDefault.Size = new System.Drawing.Size(119, 37);
+            this.btnDefault.TabIndex = 10;
+            this.btnDefault.Text = "Domyślne";
+            this.btnDefault.UseVisualStyleBackColor = true;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(205, 459);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(119, 37);
+            this.btnRefresh.TabIndex = 11;
+            this.btnRefresh.Text = "Odśwież";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(330, 459);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(119, 37);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Anuluj";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(519, 560);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Opcje.ResumeLayout(false);
+            this.Opcje.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Terminal;
+        private System.Windows.Forms.TabPage Opcje;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnDefault;
+        private System.Windows.Forms.ComboBox cbStopBits;
+        private System.Windows.Forms.ComboBox cbParityBits;
+        private System.Windows.Forms.ComboBox cbDataBits;
+        private System.Windows.Forms.ComboBox cbBaud;
+        private System.Windows.Forms.ComboBox cbName;
     }
 }
 
