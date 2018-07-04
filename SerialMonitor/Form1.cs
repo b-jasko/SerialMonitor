d249d3906809d0d534f6dff74f83126ea770791e
b-jasko <bjasko97@gmail.com>
2018-07-04 19:03:43 +0200

    Add options enter event handler

--- SerialMonitor/Form1.cs
+++ SerialMonitor/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SerialMonitor
 {
     public partial class Form1 : Form
     {
+        SerialPort port;
+
         public Form1()
         {
             InitializeComponent();
+            port = new SerialPort();
+            port.ReadTimeout = 500;
+            port.WriteTimeout = 500;
+            Opcje.Enter += new EventHandler(Opcje_Enter);
+
+        }
+
+        void Opcje_Enter(object sender, EventArgs e)
+        {
+            this.cbName.Items.Clear();
+            this.cbParityBits.Items.Clear();
+            this.cbStopBits.Items.Clear();
+            foreach (String s in SerialPort.GetPortNames()) this.cbName.Items.Add(s);
+            foreach (String s in Enum.GetNames(typeof(Parity))) this.cbParityBits.Items.Add(s);
+            foreach (String s in Enum.GetNames(typeof(StopBits))) this.cbStopBits.Items.Add(s);
+
+            cbName.Text = port.PortName.ToString();
+            cbBaud.Text = port.BaudRate.ToString();
+            cbDataBits.Text = port.DataBits.ToString();
+            cbParityBits.Text = port.Parity.ToString();
+            cbStopBits.Text = port.StopBits.ToString();
         }
     }
 }
